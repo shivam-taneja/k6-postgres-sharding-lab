65f95cb66e4023c0f95638c8f40eb91607355b81
ShivamTaneja <shiivamtaneja@gmail.com>
2026-04-13 13:24:36 +0530

    add: end point with sharding and normal

---
+++ api/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(express.json());
+
+const poolMonolith = new Pool({
+  connectionString: "postgres://postgres:password@localhost:5430/postgres",
+});
+const poolShard1 = new Pool({
+  connectionString: "postgres://postgres:password@localhost:5431/postgres",
+});
+const poolShard2 = new Pool({
+  connectionString: "postgres://postgres:password@localhost:5432/postgres",
+});
+
+const SIMULATED_QUERY = "SELECT pg_sleep(0.05);";
+
+app.get("/monolith", async (req, res) => {
+  try {
+    await poolMonolith.query(SIMULATED_QUERY);
+    res.status(200).send("Monolith Success");
+  } catch (err) {
+    res.status(500).send("Database Overloaded");
+  }
+});
+
+app.get("/sharded", async (req, res) => {
+  const userId = parseInt(req.headers["x-user-id"] || "1", 10);
+
+  try {
+    if (userId % 2 === 0) {
+      await poolShard1.query(SIMULATED_QUERY);
+    } else {
+      await poolShard2.query(SIMULATED_QUERY);
+    }
+    res.status(200).send("Sharded Success");
+  } catch (err) {
+    res.status(500).send("Database Overloaded");
+  }
+});
+
+app.listen(3000, () => console.log("API running on http://localhost:3000"));
